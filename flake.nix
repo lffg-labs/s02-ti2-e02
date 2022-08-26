9ddb724dc3b2fe823477a7cbd736f4074910ec66
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-08-26 09:21:15 -0300

    feat(db): add initial `Database` lib and Nix support

--- flake.nix
+++ flake.nix
@@ -13,6 +13,36 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+    in let
+      hooks = pkgs: {
+        jdk = ''
+          export JDK_HOME=${pkgs.jdk}
+        '';
+        postgres = ''
+          export DBROOT="$(pwd)/.postgresql"
+          export PGHOST=localhost
+          export PGPORT=5432
+          export PGDATA="$DBROOT/data"
+          export PGDATABASE=local-postgres
+          export PGUSER=$(whoami)
+          export PGPASSWORD=postgres
+          export LOG_PATH="$DBROOT/log"
+          if [ ! -d "$DBROOT" ]; then
+            echo "Creating PostgreSQL directory..."
+            mkdir -p "$DBROOT"
+          fi
+          if [ ! -d "$PGDATA" ]; then
+            echo "Initializing PostgreSQL database..."
+            initdb $PGDATA --no-locale
+          fi
+          if [ ! -f "$DBROOT/.s.PGSQL.5432.lock" ]; then
+            echo "Starting PostgreSQL database..."
+            pg_ctl start -w \
+              -l $LOG_PATH \
+              -D $PGDATA
+          fi
+        '';
+      };
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
@@ -21,10 +51,11 @@
           jdk11
           jdk
           nodePackages.cspell
+          postgresql_13
+          alejandra
         ];
+        shellHook = with pkgs.lib;
+          concatStrings (attrValues (hooks pkgs));
       };
-      shellHook = ''
-        export JDK_HOME=${pkgs.jdk}
-      '';
     });
 }
---
+++ src/main/java/lib/Database.java
@@ -0,0 +1,40 @@
+package lib;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    public static Connection getConnection() {
+        Connection connection = null;
+        String host = env("PGHOST");
+        String database = env("PGDATABASE");
+        String user = env("PGUSER");
+        String password = env("PGPASSWORD");
+        String url = String.format("jdbc:postgresql://%s/%s", host, database);
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (connection == null) {
+            throw new RuntimeException("Missing connection");
+        }
+
+        return connection;
+    }
+
+    private static String env(String name) {
+        String val = System.getenv(name);
+        if (val == null) {
+            throw new RuntimeException(String.format(
+                    "Missing required environment variable `%s`", name));
+        }
+        return val;
+    }
+}
